7482b6e661fb737eb4c239b370c481966863cb0f
Camilo <camilosanpor@gmail.com>
2025-03-20 16:09:04 -0300

    added light and dark themes

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,19 +8,19 @@ const Footer = () => {
 			<h2 className="underline">Contact Me</h2>
 			<div className="contacts">
 				<div className="contact">
-					<img src="assets/github-mark-white.png" alt="Github icon" />
+					<img src="assets/footer/github-mark-white.png" alt="Github icon" />
 					<a href="https://github.com/CamiloSanPor">Github</a>
 				</div>
 
 				<div className="contact">
-					<img src="assets/InBug-White.png" alt="LinkedIn icon" />
+					<img src="assets/footer/InBug-White.png" alt="LinkedIn icon" />
 					<a href="https://www.linkedin.com/in/camilo-sanchez-porras">
 						LinkedIn
 					</a>
 				</div>
 
 				<div className="contact">
-					<img src="assets/gmail-logo.png" alt="Gmail icon" />
+					<img src="assets/footer/gmail-logo.png" alt="Gmail icon" />
 					<a href="mailto:camilosanpor@gmail.com">Gmail</a>
 				</div>
 			</div>
@@ -33,6 +33,12 @@ const Footer = () => {
 				</a>
 				<a href="https://www.flaticon.com/free-icons/gmail" title="gmail icons">
 					Gmail icon created by Google - Flaticon
+				</a>
+				<a href="https://www.flaticon.com/free-icons/sun" title="sun icons">
+					Sun icons created by Good Ware - Flaticon
+				</a>
+				<a href="https://www.flaticon.com/free-icons/moon" title="moon icons">
+					Moon icons created by Good Ware - Flaticon
 				</a>
 			</div>
 		</footer>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,14 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router";
 import "./Header.css";
+import { DARK_THEME, LIGHT_THEME } from "../../constants/constants";
+import Sun from "../../assets/header/sun.png";
+import Moon from "../../assets/header/moon.png";
 
 const Header = () => {
+	const [theme, setTheme] = useState(
+		() => localStorage.getItem("theme") || DARK_THEME
+	);
+
+	const [icon, setIcon] = useState();
+
+	useEffect(() => {
+		if (theme === LIGHT_THEME) {
+			document.body.classList.remove(DARK_THEME);
+			document.body.classList.add(LIGHT_THEME);
+			setIcon(Sun);
+		} else {
+			document.body.classList.remove(LIGHT_THEME);
+			document.body.classList.add(DARK_THEME);
+			setIcon(Moon);
+		}
+		localStorage.setItem("theme", theme);
+	}, [theme]);
+
+	const toggleTheme = () => {
+		if (theme === DARK_THEME) {
+			setTheme(LIGHT_THEME);
+		} else {
+			setTheme(DARK_THEME);
+		}
+	};
+
 	return (
 		<nav>
 			<div>
 				<NavLink to="/">Home</NavLink>
 				<NavLink to="/about">About</NavLink>
 				<NavLink to="/projects">Projects</NavLink>
+				<button onClick={toggleTheme} className="theme-toggle">
+					<img src={icon} alt="" />
+				</button>
 			</div>
 		</nav>
 	);
