5801f9c07cf60fabe3e9cd4f65d278c590254c13
Camilo <camilosanpor@gmail.com>
2025-03-20 23:26:54 -0300

    even more logs!

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -37,6 +37,9 @@ router.get("/weather", async (req, res) => {
 		if (data.cod !== 200) {
 			return res.status(404).json({ error: "City not found" });
 		}
+		confirm.log(data.name);
+		confirm.log(data.main.temp);
+		confirm.log(data.main.humidity);
 		res.json({
 			city: data.name,
 			temperature: data.main.temp,
